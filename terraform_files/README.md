8637e1107557a7644e2532240871afbd6fdeeb49
hruday <kolipakahruday@gmail.com>
2024-08-14 15:58:54 +0530

    commiting terraform files

---
+++ terraform_files/README.md
@@ -0,0 +1,12 @@
+### TERRAFORM FILES
+---
+
+![image]![Tools](https://github.com/user-attachments/assets/07250399-4e2e-4afc-843b-8bd5cf059e61)
+
+#### The above Terraform files will create 'Jenkins' & 'K8s' servers
+#### Master-Server --> Git, Maven, Docker, Trivy, Ansible
+#### Node-Server --> Docker, K8s (Kubeadm)
+
+##### *NOTE*:
+1. Create a 'My_key.pem' from AWS EC2 console 
+2. Save the key file in the same location as your terraform code
---
+++ terraform_files/main.tf
@@ -0,0 +1,147 @@
+# SERVER1: 'MASTER-SERVER' (with Jenkins, Maven, Docker, Ansible, Trivy)
+# STEP1: CREATING A SECURITY GROUP FOR JENKINS SERVER
+# Description: Allow SSH, HTTP, HTTPS, 8080, 8081
+resource "aws_security_group" "my_security_group1" {
+  name        = "my-security-group1"
+  description = "Allow SSH, HTTP, HTTPS, 8080 for Jenkins & Maven"
+
+  # SSH Inbound Rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # SSH Outbound Rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# STEP2: CREATE AN JENKINS EC2 INSTANCE USING EXISTING PEM KEY
+# Note: i. First create a pem-key manually from the AWS console
+#      ii. Copy it in the same directory as your terraform code
+resource "aws_instance" "my_ec2_instance1" {
+  ami                    = "ami-0cf10cdf9fcd62d37"
+  instance_type          = "t2.medium"
+  vpc_security_group_ids = [aws_security_group.my_security_group1.id]
+  key_name               = "HRUDAY-KEYPAIR" # paste your key-name here, do not use extension '.pem'
+
+  # Consider EBS volume 30GB
+  root_block_device {
+    volume_size = 30    # Volume size 30 GB
+    volume_type = "gp2" # General Purpose SSD
+  }
+
+  tags = {
+    Name = "MASTER-SERVER"
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    # wait for 1min before EC2 initialization
+    sleep 60
+    sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+    sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+    sudo yum install -y apache-maven
+    sudo yum install java-1.8.0-devel -y
+  EOF
+
+  # STEP3: USING REMOTE-EXEC PROVISIONER TO INSTALL TOOLS
+  provisioner "remote-exec" {
+    # ESTABLISHING SSH CONNECTION WITH EC2
+    connection {
+      type        = "ssh"
+      private_key = file("DevopsProject1/terraform_files/HRUDAY-KEYPAIR.pem") # replace with your key-name 
+      user        = "ec2-user"
+      host        = self.public_ip
+    }
+
+    inline = [
+      # wait for 200sec before EC2 initialization
+      "sleep 200",
+      # Install Git 
+      "sudo yum install git -y",
+
+      # Install Jenkins 
+      # REF: https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
+      "sudo yum install java-17-amazon-corretto -y",
+      "sudo yum install jenkins -y",
+      "sudo systemctl enable jenkins",
+      "sudo systemctl start jenkins",
+
+      # Install Docker
+      # REF: https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/install-docker.html
+      "sudo yum update -y",
+      "sudo yum install docker -y",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo usermod -aG docker jenkins",
+      # To avoid below permission error
+      # Got permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock
+      "sudo chmod 666 /var/run/docker.sock",
+
+      # Install Trivy
+      # REF: https://aquasecurity.github.io/trivy/v0.18.3/installation/
+      "sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm",
+
+      # Install Ansible
+      "sudo yum update -y",
+      "sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y",
+      "sudo yum install git python python-devel python-pip openssl ansible -y",
+    ]
+  }
+}
+
+# STEP3: OUTPUT PUBLIC IP OF EC2 INSTANCE
+output "ACCESS_YOUR_JENKINS_HERE" {
+  value = "http://${aws_instance.my_ec2_instance1.public_ip}:8080"
+}
+
+output "Jenkins_Initial_Password" {
+  value = "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
+}
+
+# STEP4: OUTPUT PUBLIC IP OF EC2 INSTANCE
+output "MASTER_SERVER_PUBLIC_IP" {
+  value = aws_instance.my_ec2_instance1.public_ip
+}
+
+# STEP5: OUTPUT PRIVATE IP OF EC2 INSTANCE
+output "MASTER_SERVER_PRIVATE_IP" {
+  value = aws_instance.my_ec2_instance1.private_ip
+}
